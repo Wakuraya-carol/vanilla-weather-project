461fcc707a9541502d07475773f0d5927b79b7ff
Wakuraya-carol <carolinewakuraya@gmail.com>
2024-12-11 12:28:36 +0300

    update html

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
 
   cityElement.innerHTML = response.data.city;
+  descriptionElement = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
   let apiKey = "047dao5425tbfd24a0939f3bc93f8190";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metrics`;
+
   axios.get(apiUrl).then(refreshWeather);
 }
 
